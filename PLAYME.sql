2967952079d979a4a5c6b278fbdc428d2b00465b
Bonzar <nva112002@gmail.com>
2020-08-23 09:06:17 +0300

    Upload all project files

---
+++ PLAYME.sql
@@ -0,0 +1,16 @@
+-- The database creation script
+
+-- the structures of the database tables if they have not been uploaded to the complete database
+\. TABLES.sql
+
+-- the creation of foreign keys
+\. FOREIGN_KEYS.sql
+
+-- adding triggers
+\. TRIGGERS.sql
+
+-- creating views
+\. VIEWS.sql
+
+-- creating functions and procedures
+\. FUNCTIONS.sql
